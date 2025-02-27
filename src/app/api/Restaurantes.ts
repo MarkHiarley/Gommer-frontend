c2ab1ccc38afaea48def0ad569fad1ac517fe51e
MarkHiarley <hiarleycostamarley@gmail.com>
2025-02-27 20:56:50 -0300

    api-backend

--- src/app/api/Restaurantes.ts
+++ src/app/api/Restaurantes.ts
@@ -1,19 +1,26 @@
-export const verifyToken = async (token: string) => {
+
+export const pegarRestaurantes = async() => {
     try {
-        const response = await fetch('http://localhost:3001/api/auth', {
-            method: 'POST',
+        const response = await fetch('http://localhost:3001/api/restaurantes', {
+            method: 'GET',
             credentials: 'include',
             headers: {
                 'Content-Type': 'application/json'
+                
             }
         });
         if (!response.ok) {
             throw new Error(`Erro na requisição: ${response.statusText}`);
         }
-        const result = await response.json();
-        return result;
+
+       
+        const data = response.json();
+        
+        return data
+    
+    
     } catch (error) {
         console.error('Erro:', error);
-        throw error;
+        return [];
     }
-};
+}
