79e0ee097b014f55d20888ac8ec7bc1dcb6ba8fa
MarkHiarley <hiarleycostamarley@gmail.com>
2025-02-28 08:30:50 -0300

    basic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Restaurantes from "./components/NavBar";
 import TitleRango from "./components/Title";
 import SearchBar from "./components/SeachBar";
 import BoxRestaurantes from "./components/BoxRestaurantes";
-import IconRestaurante from "./components/IconRestaurante";
+
 
 interface HomeProps {
   token: string
