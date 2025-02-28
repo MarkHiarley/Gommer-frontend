4abe8bcb5b3c6b47545894c3af652c88bd84c613
MarkHiarley <hiarleycostamarley@gmail.com>
2025-02-28 08:31:31 -0300

    icon do box de restaurantes

--- src/app/components/IconRestaurante.tsx
+++ src/app/components/IconRestaurante.tsx
@@ -8,7 +8,7 @@ const IconRestaurante = () => {
 
                 <div className="flex flex-row p-3 border shadow-xl w-96" >
 
-                    <img src="https://i.ytimg.com/vi/tPp9zkjfj7Q/hqdefault.jpg?sqp=-oaymwExCNACELwBSFryq4qpAyMIARUAAIhCGAHwAQH4Ac4FgALQBYoCDAgAEAEYfyA1KCUwDw==&rs=AOn4CLB1ZWTVP9G0xaawlrs6bwmgOVMyxA" alt="Foto do restaurante" className="w-16 h-16 object-cover" />
+                    <img src="" alt="Foto do restaurante" className="w-16 h-16 object-cover" />
 
                     <div className="flex flex-col w-11/12">
                         <span className="flex justify-center">Nome restaurante</span>
